29ee9aa563b7217cade9654519173b5363f9b3ca
Marina Hiuk <mlazor@mail.ru>
2021-02-12 17:27:48 +0100

    block maps added

--- src/js/main.min.js
+++ src/js/main.min.js
@@ -9,6 +9,38 @@ function init() {
 
   document.querySelector('.burger-menu').addEventListener('click', function() {
    toggleClasses();
-  })
+  });
+
+
 }
 
+
+
+// The latitude and longitude of your business / place
+let position = [39.739235, -104.990250];
+ 
+function showGoogleMaps() {
+ 
+let latLng = new google.maps.LatLng(position[0], position[1]);
+ 
+let mapOptions = {
+zoom: 13, // initialize zoom level - the max value is 21
+streetViewControl: false, // hide the yellow Street View pegman
+scaleControl: true, // allow users to zoom the Google Map
+mapTypeId: google.maps.MapTypeId.ROADMAP,
+center: latLng
+};
+ 
+map = new google.maps.Map(document.getElementsByClassName('maps'),
+mapOptions);
+ 
+// Show the default red marker at the location
+marker = new google.maps.Marker({
+position: latLng,
+map: map,
+draggable: false,
+animation: google.maps.Animation.DROP
+});
+}
+ 
+google.maps.event.addDomListener(window, 'load', showGoogleMaps);
